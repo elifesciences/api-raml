0bd1473f1dd52676b6ce0a18527fd2c4e3636566
Chris Wilkinson <c.wilkinson@elifesciences.org>
2016-06-09 10:03:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+eLife Sciences API RAML
+=======================
+
+This repo contains [RAML 1.0 specifications](https://github.com/raml-org/raml-spec/blob/master/versions/raml-10/raml-10.md) for the eLife Sciences API.
---
+++ compile.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var fs = require('fs-extra');
+var path = require('path');
+var ramlParser = require('raml-1-parser');
+var recursive = require('recursive-readdir');
+var refParser = require('json-schema-ref-parser');
+
+fs.removeSync('./dist');
+fs.mkdirsSync('./dist/model');
+
+recursive('./src/model', function (err, files) {
+    var promises = [];
+
+    for (var i = 0, len = files.length; i < len; i++) {
+        var file = files[i];
+        if ('yaml' !== file.split('.').pop()) {
+            continue;
+        }
+
+        var parser = new refParser();
+
+        (function (file) {
+            promises.push(parser.bundle(file).then(function (schema) {
+                var jsonFile = './dist/model/' + file.slice(10, (file.length - 4)) + 'json';
+                var json = JSON.stringify(schema, null, 4) + '\n';
+                fs.writeFileSync(jsonFile, json);
+            }));
+        }(file))
+    }
+
+    Promise.all(promises).then(function () {
+        var raml = fs.readFileSync('./src/api.raml', 'utf8').replace(/!include \.\.\/dist\//g, '!include ');
+
+        fs.writeFileSync('./dist/api.raml', raml);
+
+        try {
+            ramlParser.loadRAMLSync(path.resolve(__dirname, './dist/api.raml'), [], {rejectOnErrors: true});
+        } catch (error) {
+            console.error(error);
+        }
+
+        fs.copy('./node_modules/api-console/dist/fonts', './dist/fonts');
+        fs.copy('./node_modules/api-console/dist/img', './dist/img');
+        fs.copy('./node_modules/api-console/dist/scripts', './dist/scripts');
+        fs.copy('./node_modules/api-console/dist/styles', './dist/styles');
+        fs.copy('./src/index.html', './dist/index.html');
+        fs.copy('./src/favicon.ico', './dist/favicon.ico');
+    });
+});
